6223aee6151974d56e9acc286fa1e5f20e65fb68
Tikhon Gritsenko <tikhongritsenko@gmail.com>
2025-03-31 13:39:16 +0300

    first file

--- SQL1_SQL_Boot_camp/Team01/solution/Tikhon_21Tasks_Team01.sql
+++ SQL1_SQL_Boot_camp/Team01/solution/Tikhon_21Tasks_Team01.sql
@@ -34,37 +34,44 @@ order by
 insert into currency values (100, 'EUR', 0.85, '2022-01-01 13:29');
 insert into currency values (100, 'EUR', 0.79, '2022-01-08 13:29');
 
-select 
-    coalesce(u.name, 'not defined') as name,
-    coalesce(u.lastname, 'not defined') as lastname,
-    c.name as currency_name,
-    (b.money * coalesce(rate.rate_to_usd, 1)) as currency_in_usd
-from balance b
-join currency c on b.currency_id = c.id
-left join "user" u on b.user_id = u.id
-cross join lateral(
-    select rate_to_usd
-    from(
-            (select c1.rate_to_usd       
-             from currency c1
-             where c1.id = b.currency_id and c1.updated <= b.updated
-             order by c1.updated desc
-             limit 1)
-            union all
-            (select c2.rate_to_usd
-             from currency c2
-             where c2.id = b.currency_id and c2.updated > b.updated
-             order by c2.updated asc
-             limit 1)
-    ) as sub
-    limit 1
-) as rate
-group by
-    coalesce(u.name, 'not defined'),
-    coalesce(u.lastname, 'not defined'),
-    c.name,
-    (b.money * coalesce(rate.rate_to_usd, 1))
-order by 
-    name desc,
-    lastname,
-    currency_name;
+WITH crn_tmp AS (
+    SELECT
+        crn.id,
+        crn.rate_to_usd,
+        crn.name,
+        LAG(crn.updated) OVER(PARTITION by crn.id ORDER by crn.updated) prev_date,
+        LEAD(crn.updated) OVER(PARTITION by crn.id ORDER by crn.updated) next_date,
+        crn.updated cur_date
+    FROM currency crn
+    ),
+    crn AS (
+   SELECT
+        id,
+        rate_to_usd,
+        name,
+        COALESCE(prev_date, '1000-01-01') start_date,
+        cur_date end_date
+    FROM crn_tmp
+    WHERE prev_date IS NULL
+    UNION ALL
+    SELECT
+        id,
+        rate_to_usd,
+        name,
+        cur_date start_date,
+        COALESCE(next_date, '3000-01-01') end_date
+    FROM crn_tmp
+    )
+SELECT
+    COALESCE(us.name, 'not defined') name,
+    COALESCE(us.lastname, 'not defined') lastname,
+    COALESCE(crn.name, 'not defined') currency_name,
+    crn.rate_to_usd * bl.money currency_in_usd
+FROM balance bl
+JOIN crn
+    ON bl.currency_id = crn.id
+    AND bl.updated > crn.start_date
+    AND bl.updated <= crn.end_date
+LEFT OUTER JOIN "user" us
+    ON us.id = bl.user_id
+ORDER by 1 desc, 2, 3;
